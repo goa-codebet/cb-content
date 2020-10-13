f1038312ded2be5cc0b2cf701e30f992c2ee0e5b
Robert Ahlberg <rob.ahlberg@gmail.com>
2020-10-13 11:14:57 +0200

    Run prettier on utils

--- utils.js
+++ utils.js
@@ -1,31 +1,36 @@
 /**
-  *
-  */
+ *
+ */
 async function createEntry(client, type, data) {
-  return await client.createEntry(type, data)
+  return await client.createEntry(type, data);
 }
 
 /**
-  *
-  */
-async function findOrCreateEntry(client, type, search, data, onlyCreate = false) {
-  const key = `Find or create ${type}: '${search}'`
-  console.time(key)
-  
-  let entry = null
-  
+ *
+ */
+async function findOrCreateEntry(
+  client,
+  type,
+  search,
+  data,
+  onlyCreate = false
+) {
+  const key = `Find or create ${type}: '${search}'`;
+  console.time(key);
+
+  let entry = null;
+
   try {
-    if (entry_cache[type+search]) {
-      console.log(`Found in cache`)
-      console.timeEnd(key)
-      return entry_cache[type+search]
+    if (entry_cache[type + search]) {
+      console.log(`Found in cache`);
+      console.timeEnd(key);
+      return entry_cache[type + search];
     }
     entry = await client.getEntries({
-      'content_type': type,
-      ['fields.identifier']: search
-    })
-    if (!entry.items || entry.items.length === 0)
-      throw('NotFound');
+      content_type: type,
+      ['fields.identifier']: search,
+    });
+    if (!entry.items || entry.items.length === 0) throw 'NotFound';
 
     if (onlyCreate) {
       console.log(`Entry found, abort due to onlyCreate == true.`);
@@ -34,28 +39,25 @@ async function findOrCreateEntry(client, type, search, data, onlyCreate = false)
 
     entry = entry.items[0];
 
-    console.log(`Found on remote`)
-    console.timeEnd(key)
-  }
-  catch(err) {
-    if (!err.request || ! err.request.res)
-      console.log(err)
+    console.log(`Found on remote`);
+    console.timeEnd(key);
+  } catch (err) {
+    if (!err.request || !err.request.res) console.log(err);
 
     if (err === 'NotFound' || err.request.res.statusCode === 404) {
-      entry = await createEntry(client, type, data)
-      console.log(`Not found, created.`)
-      console.timeEnd(key)
+      entry = await createEntry(client, type, data);
+      console.log(`Not found, created.`);
+      console.timeEnd(key);
     }
   }
 
-  if (entry.isUpdated() || !entry.isPublished())
-    entry = entry.publish()
+  if (entry.isUpdated() || !entry.isPublished()) entry = entry.publish();
 
-  entry_cache[type+_.kebabCase(search)] = entry
-  return entry
+  entry_cache[type + _.kebabCase(search)] = entry;
+  return entry;
 }
 
 exports = {
-  findOrCreateEntry, 
+  findOrCreateEntry,
   createEntry,
-}
+};
