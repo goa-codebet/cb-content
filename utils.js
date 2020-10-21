109c60dade4c4c8a03a3806a787e74fd1c57e7f4
Robert Ahlberg <rob.ahlberg@gmail.com>
2020-10-21 15:17:36 +0200

    So far

--- utils.js
+++ utils.js
@@ -61,7 +61,105 @@ async function findOrCreateEntry(
   return entry;
 }
 
+async function createAsset(
+  client,
+  url,
+  fileName,
+  contentType,
+  locale = 'en-US'
+) {
+  console.log('createAsset');
+  const asset = await client.createAsset({
+    fields: {
+      title: {
+        [locale]: fileName,
+      },
+      file: {
+        [locale]: {
+          contentType,
+          fileName,
+          upload: url,
+        },
+      },
+    },
+  });
+
+  return asset;
+}
+
+async function findOrCreateAsset(client, url, name, locale = 'en-US') {
+  console.log('findOrCreateAsset');
+  console.log('url: ', url);
+  console.log('name: ', name);
+
+  if (!url) return false;
+
+  const parts = url.split('/');
+  if (!parts) return false;
+
+  const fileName = name || parts[parts.length - 1];
+  if (!fileName) return false;
+
+  const extention = fileName.split('.')[fileName.split('.').length - 1];
+  if (!extention) return false;
+
+  let contentType = 'image/' + extention;
+  if (extention === 'jpg') contentType = 'image/jpeg';
+
+  console.time(`Find or create asset: '${fileName}'`);
+
+  let asset = null;
+
+  try {
+    if (entry_cache['asset' + _.kebabCase(fileName)]) {
+      console.timeEnd(`Find or create asset: '${fileName}'`);
+      return entry_cache['asset' + _.kebabCase(fileName)];
+    }
+
+    asset = await client.getAssets({
+      'fields.file.fileName': fileName,
+    });
+
+    if (!asset.items || asset.items.length === 0) throw 'NotFound';
+
+    asset = asset.items[0];
+
+    console.timeEnd(`Find or create asset: '${fileName}'`);
+  } catch (err) {
+    if (!err.request || !err.request.res) console.log(err);
+
+    if (err === 'NotFound' || err.request.res.statusCode === 404) {
+      asset = await createAsset(
+        client,
+        'http:' + url,
+        fileName,
+        contentType,
+        locale
+      );
+      asset = await asset.processForAllLocales();
+      console.timeEnd(`Find or create asset: '${fileName}'`);
+    }
+  }
+
+  // Cannot publish?? VersionMissMatch?
+  if (asset.isUpdated() || !asset.isPublished()) asset = await asset.publish();
+
+  entry_cache['asset' + _.kebabCase(fileName)] = asset;
+  return asset;
+}
+
+createLink = item => ({
+  sys: {
+    type: 'Link',
+    linkType: item.sys.type,
+    id: item.sys.id,
+  },
+});
+
 module.exports = {
   findOrCreateEntry,
   createEntry,
+  findOrCreateAsset,
+  createAsset,
+  createLink,
 };
